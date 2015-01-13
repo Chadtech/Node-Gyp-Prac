3e9771da049c97f52423c6a6c172afe95e6ebd7b
Chadtech <chadstearns1@gmail.com>
2015-01-13 11:39:25 -0700

    Properly named files, Added README

--- DANKMEMES.js
+++ DANKMEMES.js
@@ -2,6 +2,7 @@ var DANKMEMES = require('./build/Release/DANKMEMES');
 // This ------------------------------------^ has to be the same as target_nane in binding.gyp
 
 DANKMEMES.dAnKmEmEs();
+
 DANKMEMES = DANKMEMES.DANKMEMES("DANK MEMES");
 
-console.log('This is DANKMEMES ', DANKMEMES);
+console.log('DANK MEMES', DANKMEMES);
